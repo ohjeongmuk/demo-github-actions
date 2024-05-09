886aae9b60723ffd96cc1319ca12b1562f7eda68
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-05-09 08:59:21 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import prefetch from "@astrojs/prefetch";
 
 export default defineConfig({
   integrations: [tailwind(), sitemap(), prefetch()],
-  site: "https://adulbrich.github.io",
+  site: "https://github.com/ohjeongmuk/FullStack-Web-Dev-2024",
   base: "/demo-github-actions",
 });
